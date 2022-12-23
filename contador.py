1dc478a6f951edbe87ecef6d5cae03bf001cd787
nmd20 <nmd20@alu.ua.es>
2022-12-23 11:34:22 +0100

    v.1.0.1

--- contador.py
+++ contador.py
@@ -1,7 +1,9 @@
 import csv
 import string
 
-text = open('declaration.txt').read()
+print("¿Cómo se llama el archivo?")
+nombre = input()
+text = open(nombre).read()
 words = text.split()
 num_words = len(words)
 print("el archivo contiene: " + str(num_words) + "palabras")
